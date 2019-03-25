2af9d5ae06302166f8e37aa3f064c5b51eb54369
tony <tonytenorioc@gmail.com>
2019-03-24 20:21:48 -0600

    Gallery Section completed

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ $(document).ready(function() {
     $('.nav-button').click(function() {
         $('.nav-button').toggleClass('change');
     });
+
+    $('.gallery-list-item').click(function() {
+        let value = $(this).attr('data-filter');
+        if(value === 'all') {
+          $('.filter').show(300);
+        } else {
+          $('.filter').not('.' + value).hide(300);
+          $('.filter').filter('.' + value).show(300);
+        }
+      });
+    
+      $('.gallery-list-item').click(function() {
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
    
 });
