03e8f33a6c11898cd0a404cce8f8a79bb4f02cfb
tony <tonytenorioc@gmail.com>
2019-03-28 11:14:42 -0600

    Beauty CSS

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
         $('.nav-button').toggleClass('change');
     });
 
+    $(window).scroll(function() {
+      let position = $(this).scrollTop();
+
+      if(position >=350) {
+        $('.mission-text').addClass('fromLeft');
+        $('.camera-img').addClass('fromRight');
+      } else {
+        $('.mission-text').removeClass('fromLeft');
+        $('.camera-img').removeClass('fromRight');
+      }
+    });
+
     $('.gallery-list-item').click(function() {
         let value = $(this).attr('data-filter');
         if(value === 'all') {
@@ -16,6 +28,19 @@ $(document).ready(function() {
     
       $('.gallery-list-item').click(function() {
         $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
+
+    $(window).scroll(function() {
+      let position = $(this).scrollTop();
+      if(position >= 4300) {
+        $('.card-1').addClass('moveFromLeft');
+        $('.card-2').addClass('moveFromBottom');
+        $('.card-3').addClass('moveFromRight');
+      } else {
+        $('.card-1').removeClass('moveFromLeft');
+        $('.card-2').removeClass('moveFromBottom');
+        $('.card-3').removeClass('moveFromRight');
+      }
     });
 
     $('#year').text(new Date().getFullYear());
